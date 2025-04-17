c302f1cf721cf0553078d37a9ae5973c8815049f
Lethienbao2004 <markle1903@icloud.com>
2025-04-17 00:15:21 -0700

    Initial Commit

---
+++ SneakyPeaky/SneakyPeakyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneakyPeakyApp.swift
+//  SneakyPeaky
+//
+//  Created by Mark Le on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SneakyPeakyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
